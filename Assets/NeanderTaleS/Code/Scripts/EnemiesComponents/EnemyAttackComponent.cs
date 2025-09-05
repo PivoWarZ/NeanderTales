af1bf3886e0d30c1860b0dd55f4d9fdd065837a8
Dmitry <academik87@gmail.com>
2025-09-05 16:41:49 +0300

    no commit

--- Assets/NeanderTaleS/Code/Scripts/EnemiesComponents/EnemyAttackComponent.cs
+++ Assets/NeanderTaleS/Code/Scripts/EnemiesComponents/EnemyAttackComponent.cs
@@ -1,32 +1,57 @@
 using System;
 using NeanderTaleS.Code.Scripts.Condition;
+using NeanderTaleS.Code.Scripts.Services;
 using UnityEngine;
 
 namespace NeanderTaleS.Code.Scripts.EnemiesComponents
 {
     public class EnemyAttackComponent: MonoBehaviour
     {
-        public event Action AttackRequest;
-        public event Action AttackAction;
-        public event Action AttackEvent;
+        public event Action OnAttackRequest;
+        public event Action OnAttackAction;
+        public event Action OnAttackEvent;
         
-        [SerializeField] private float _attackColdown = 1f;
-        [SerializeField] private bool _canAttack = true;
+        [SerializeField] private float _attackColdown = 2f;
+        [SerializeField] private bool _canAttack = false;
+        private Timer _attackTimer;
         private CompositeCondition _condition = new ();
 
         private void Awake()
         {
             _condition.AddCondition(() => _canAttack);
+            
+            _attackTimer = new Timer();
+            _attackTimer.IsLoop = false;
+            _attackTimer.Duration = _attackColdown;
+            
+            _attackTimer.OnEnded += AttackReady;
+        }
+
+        private void OnEnable()
+        {
+            _attackTimer.Start();
+        }
+
+        private void AttackReady()
+        {
+            _canAttack = true;
         }
 
         public void Attack()
         {
-            AttackRequest?.Invoke();
+            OnAttackRequest?.Invoke();
 
             if (_condition.IsTrue())
             {
-                AttackAction?.Invoke();
+                OnAttackAction?.Invoke();
             }
+        }
+
+        public void AttackEVent()
+        {
+            OnAttackEvent?.Invoke();
+            _canAttack = false;
+            _attackTimer.Start();
         }
 
         public void SetCondition(Func<bool> condition)
@@ -37,6 +62,11 @@ namespace NeanderTaleS.Code.Scripts.EnemiesComponents
         public void RemoveCondition(Func<bool> condition)
         {
             _condition.RemoveCondition(condition);
+        }
+
+        private void OnDestroy()
+        {
+            _attackTimer.OnEnded -= AttackReady;
         }
     }
 }
